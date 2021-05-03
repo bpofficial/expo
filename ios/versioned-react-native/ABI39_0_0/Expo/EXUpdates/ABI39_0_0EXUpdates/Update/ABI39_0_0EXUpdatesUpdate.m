1a6f0b8c58e11536d840a25a32203ff9a5e82f07
Eric Samelson <eric@expo.io>
2021-05-03 15:56:32 -0700

    [ios] rename EXUpdatesUpdate.metadata -> manifest

--- ios/versioned-react-native/ABI39_0_0/Expo/EXUpdates/ABI39_0_0EXUpdates/Update/ABI39_0_0EXUpdatesUpdate.m
+++ ios/versioned-react-native/ABI39_0_0/Expo/EXUpdates/ABI39_0_0EXUpdates/Update/ABI39_0_0EXUpdatesUpdate.m
@@ -46,7 +46,7 @@ NS_ASSUME_NONNULL_BEGIN
   update.updateId = updateId;
   update.commitTime = commitTime;
   update.runtimeVersion = runtimeVersion;
-  update.metadata = metadata;
+  update.manifest = metadata;
   update.status = status;
   update.keep = keep;
   return update;
